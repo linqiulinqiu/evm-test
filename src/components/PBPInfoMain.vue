d29eebbd695b353101e4bfadb166dc467b1a47a1
linqiu <1131868300@qq.com>
2023-01-11 11:21:16 +0800

    revise table

--- src/components/PBPInfoMain.vue
+++ src/components/PBPInfoMain.vue
@@ -1,23 +1,26 @@
 <template>
   <el-col>
     <el-col>
-      <h4>PBP发行量</h4>
       <ul>
-        <li>合约地址：<a target="_blank" :href="tokenUrl()">{{ pbpAddress }}</a></li>
-        <li>当前发行总量：{{totalSupply}}</li>
-        <li>官方销毁总量：{{totalBurnt }}</li>
+        <li>
+          合约地址：<a target="_blank" :href="tokenUrl()">{{ pbpAddress }}</a>
+        </li>
+        <li>当前发行总量：{{ totalSupply }}</li>
+        <li>官方销毁总量：{{ totalBurnt }}</li>
       </ul>
     </el-col>
     <el-col>
-      销毁历史
-      <p v-if="burns.length==0">
-        Loading
-      </p>
-      <el-table strip border v-else :data="burns">
-        <el-table-column prop="time" label="Burn Time"></el-table-column>
-        <el-table-column prop="amount" label="PBP Amount"></el-table-column>
-        <el-table-column prop="txid" label="TX"></el-table-column> 
-        <el-table-column prop="url" label="Link"></el-table-column>
+      <p v-if="burns.length == 0">Loading</p>
+      <el-table strip v-else :data="burns" style="width: 100%">
+        <el-table-column label="withdraw history">
+          <el-table-column prop="time" label="Burn Time"></el-table-column>
+          <el-table-column prop="amount" label="PBP Amount"></el-table-column>
+          <el-table-column label="TX">
+            <template slot-scope="scope">
+              <a :href="scope.row.url">{{ scope.row.txid }}</a>
+            </template>
+          </el-table-column>
+        </el-table-column>
       </el-table>
     </el-col>
   </el-col>
@@ -29,61 +32,66 @@ import tokens from "../tokens";
 import recBurns from "../rec-burns.json";
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export default {
   name: "PBPInfoMain",
-  components: {
-  },
+  components: {},
   computed: mapState({
     bsc: "bsc",
-    burns: function (){
-      const res = []
-      for(var i in recBurns.burns){
-        const burn = recBurns.burns[i]
-        console.log('burn=',burn)
+    burns: function () {
+      const res = [];
+      for (var i in recBurns.burns) {
+        const burn = recBurns.burns[i];
+        console.log("burn=", burn);
         res.push({
-          amount: ethers.utils.formatUnits(burn.args[2],'gwei'),  // TODO: gwei 刚好对应PBP的decimals，不可用于其它Token
-          time: (new Date(burn.timestamp*1000)).toLocaleDateString('zh-CN'),  // TODO: 应从系统locale读取
+          amount: ethers.utils.formatUnits(burn.args[2], "gwei"), // TODO: gwei 刚好对应PBP的decimals，不可用于其它Token
+          time: new Date(burn.timestamp * 1000).toLocaleDateString("zh-CN"), // TODO: 应从系统locale读取
           txid: burn.transactionHash,
-          url: `https://bscscan.com/tx/${burn.transactionHash}`
-        })
+          url: `https://bscscan.com/tx/${burn.transactionHash}`,
+        });
       }
-      return res
-    }
+      return res;
+    },
   }),
   mounted() {
-    this.loadInfo()
+    this.loadInfo();
   },
   data() {
     return {
-      pbpAddress: '-',
-      totalSupply: '-',
-      totalBurnt: '-'
+      pbpAddress: "-",
+      totalSupply: "-",
+      totalBurnt: "-",
     };
   },
   methods: {
-    tokenUrl: function (){
-      if(this.pbpAddress.length>1){
-        return 'https://bscscan.com/token/'+this.pbpAddress
+    tokenUrl: function () {
+      if (this.pbpAddress.length > 1) {
+        return "https://bscscan.com/token/" + this.pbpAddress;
       }
-      return '#';
+      return "#";
     },
     loadInfo: async function () {
-      const ctr = this.bsc.ctrs['pbp']
-      this.pbpAddress = ctr.address
-      this.totalSupply = await tokens.format(this.pbpAddress,await tokens.supply(this.pbpAddress))
-      var totalBurnt = ethers.BigNumber.from(0)
-      for(var i in recBurns.burns){
-        const burn = recBurns.burns[i]
-        totalBurnt = totalBurnt.add(burn.args[2])
+      const ctr = this.bsc.ctrs["pbp"];
+      this.pbpAddress = ctr.address;
+      this.totalSupply = await tokens.format(
+        this.pbpAddress,
+        await tokens.supply(this.pbpAddress)
+      );
+      var totalBurnt = ethers.BigNumber.from(0);
+      for (var i in recBurns.burns) {
+        const burn = recBurns.burns[i];
+        totalBurnt = totalBurnt.add(burn.args[2]);
       }
-      this.totalBurnt = ethers.utils.formatUnits(totalBurnt,'gwei')
+      this.totalBurnt = ethers.utils.formatUnits(totalBurnt, "gwei");
     },
   },
 };
 </script>
-<style>
-
+<style scoped>
+.el-table {
+  text-align: center;
+}
+</style>>
 </style>
--- src/views/PBPToken.vue
+++ src/views/PBPToken.vue
@@ -1,20 +1,24 @@
 <template>
   <el-container>
-    <el-main>
-      <el-col>
-        <h3>About PBP</h3>
-        <p>在这里对PBP进行简单的介绍：</p>
-        <p>PBP的作用</p>
-        <p>PBP的发行规则</p>
-        <p>PBP的价值保障，包括回购和销毁规则以及锁定规则</p>
+    <el-main class="pbptoken">
+      <el-col class="token-info" :span="20" :offset="2">
+        <el-col :span="5" :offset="1" :xs="{ offset: 0 }">
+          <h1>PBP</h1>
+          <h3>PlotBridge Point</h3>
+        </el-col>
+        <el-col :span="18">
+          <p>在这里对PBP进行简单的介绍：</p>
+          <p>PBP的作用</p>
+          <p>PBP的发行规则</p>
+          <p>PBP的价值保障，包括回购和销毁规则以及锁定规则</p></el-col
+        >
       </el-col>
-      <hr>
-      <el-col v-if="bsc.addr">
-        <p>PBP当前发行状态</p>
+      <el-col v-if="bsc.addr" :span="20" :offset="2">
+        <h4>PBP当前发行状态</h4>
         <PBPInfoMain />
       </el-col>
-      <el-col v-else>
-          Connect wallet for a list of PBP buy-back and burn history.
+      <el-col class="text-center" v-else>
+        Connect wallet for a list of PBP buy-back and burn history.
       </el-col>
     </el-main>
   </el-container>
@@ -32,5 +36,22 @@ export default {
   }),
 };
 </script>
-<style>
+<style scoped>
+.el-main {
+  font-size: 14px;
+  font-family: "sans-serif", "monospace";
+}
+.token-info {
+  margin-top: 20px;
+  background-color: #373943d5;
+  border-radius: 10px;
+  padding: 20px 5px;
+}
+.line {
+  margin: 15px auto;
+}
+.text-center {
+  text-align: center;
+}
+</style>>
 </style>
