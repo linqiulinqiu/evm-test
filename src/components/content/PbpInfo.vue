50858dc1387dfd63857358532960517edc1d2ffc
linqiu <1131868300@qq.com>
2023-02-03 16:25:56 +0800

    add issue table. but table column have some bug

--- src/components/content/PbpInfo.vue
+++ src/components/content/PbpInfo.vue
@@ -24,6 +24,31 @@
                 </p>
               </span>
             </p>
+            <el-table :data="this.data">
+              <el-table-column
+                prop="date"
+                :label="$t('issue-date')"
+              ></el-table-column>
+              <el-table-column
+                prop="time"
+                :label="$t('issue-time')"
+              ></el-table-column>
+              <el-table-column :label="$t('issue-amount')">
+                <template slot-scope="scope">
+                  <RichNumber :data="String(scope.row.stake)"> </RichNumber>
+                </template>
+              </el-table-column>
+              <el-table-column :label="$t('issue-stage')">
+                <template slot-scope="scope">
+                  <RichNumber :data="String(scope.row.stage)"> </RichNumber>
+                </template>
+              </el-table-column>
+              <el-table-column :label="$t('issue-sum')">
+                <template slot-scope="scope">
+                  <RichNumber :data="String(scope.row.sum)"> </RichNumber>
+                </template>
+              </el-table-column>
+            </el-table>
           </el-col>
         </el-tab-pane>
         <el-tab-pane :label="this.$t('pbp-price-support')">
@@ -41,9 +66,20 @@
 </template>
 <script>
 </script>
-<style scoped>
-.pbp-rule {
-  /* overflow-y: scroll; */
-  /* height: 200px; */
-}
-</style>
+<script>
+import info from "../../pbp-amount.json";
+import RichNumber from "../lib/RichNumber.vue";
+
+export default {
+  components: {
+    RichNumber,
+  },
+  data() {
+    console.log("info", info.stageInfo);
+    return {
+      stake_stage: info.stake_stage,
+      data: info.stageInfo,
+    };
+  },
+};
+</script>
--- tools/stage-amounts.js
+++ tools/stage-amounts.js
@@ -5,7 +5,10 @@ const fs = require('fs')
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
+var info = {
+    stake_stage: '',
+    stageInfo:[]
+}
 Date.prototype.addDays = function(days) {
     var date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
@@ -18,21 +21,41 @@ async function load_start(bsc, ctr){
     return date
 }
 
-async function main(){
+async function main() {
+    const amountPath = "pbp-amount.json"
     const wallet = ethers.Wallet.createRandom()
     const bsc = await pbw.connect_rpc(false, wallet.privateKey, 'https://rpc.ankr.com/bsc') 
     const pbp = bsc.ctrs['pbp']
-    var date = await load_start(bsc,pbp)
+    var date = await load_start(bsc, pbp)
     const mintCaps = 1000*1000*1000;
-    var sum = 145*1000*1000
-    console.log('sum=', sum)
-    for(var i=90;i<=1440;i*=2){
+    var sum = 145 * 1000 * 1000
+    
+    if (fs.existsSync(amountPath)) {
+        info=JSON.parse(fs.readFileSync(amountPath))
+    }
+    var infoP = []
+    for (var i = 90; i <= 1440; i *= 2){
+        var item = {}
         const stake_amount = mintCaps*72/1000;
         const stage_amount = mintCaps*99/1000;
         sum += stake_amount+stage_amount; 
-        console.log('halving=', date, 'stage_amount=', stage_amount/i, 'stake_amount =', stake_amount/i, 'sum=', sum)
         date = date.addDays(i)
+        item['date'] = date
+        item['time'] = i
+        item['stake'] = stake_amount/i
+        item['stage'] = stage_amount / i
+        item['sum'] = sum
+        infoP.push(item)
     }
+    info.stake_stage = sum
+    info.stageInfo = infoP
+    const data = JSON.stringify(info)
+    fs.writeFileSync(amountPath, data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+    })
 }
 
 main()
